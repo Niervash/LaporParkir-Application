46ee035c6b1807c81c4ef702cd1615d397aa72d8
GLynn <febriantomanangi@gmail.com>
2024-10-04 22:11:15 +0800

    change config database

--- config/config.js
+++ config/config.js
@@ -8,19 +8,5 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql'
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
   }
 }
