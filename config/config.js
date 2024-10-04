0c7bc75d0d3a3feb021c2dc166e2428225595231
GLynn <febriantomanangi@gmail.com>
2024-10-05 00:37:22 +0800

    fix error on render

--- config/config.js
+++ config/config.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
-console.log("DB_URL:", process.env.DB_URL);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-
 
 module.exports = {
   development: { 
-    use_env_variable: 'DB_URL', 
+    use_env_variable: 'DB_URL',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+  },
+  production: {
+    use_env_variable: 'DB_URL',
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
