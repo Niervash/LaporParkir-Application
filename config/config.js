e39313b3cdc28644d4a3ab119b58830832dccd86
GLynn <febriantomanangi@gmail.com>
2024-10-04 22:17:52 +0800

    add in config use_env_variable

--- config/config.js
+++ config/config.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   development: { 
-    username : process.env.DB_USER,
+    use_env_variable: false, // Atur ke false atau hapus jika tidak digunakan
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql'
   }
-}
+};
