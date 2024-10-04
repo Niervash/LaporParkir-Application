4dd4cad0eb33561b05712865a25cc4d2d73aa2c7
GLynn <febriantomanangi@gmail.com>
2024-10-05 00:25:27 +0800

    fix error env variable on render

--- config/config.js
+++ config/config.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
+console.log("DB_URL:", process.env.DB_URL);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 
 module.exports = {
   development: { 
-    use_env_variable: process.env.DB_URL, 
+    use_env_variable: 'DB_URL', 
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
