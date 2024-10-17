4452e535c681e29db513176b3d64ce48042815a8
GLynn <febriantomanangi@gmail.com>
2024-10-17 14:25:55 +0800

    ubah model petugas

--- middleware/adminApproval.js
+++ middleware/adminApproval.js
@@ -4,43 +4,27 @@ const {petugas_parkir} = require('../models');
 module.exports = {
     approvePetugasParkir: async (req, res, next) => {
 
-        const {id} = req.params;
-        const {action} = req.body
-    
-    
-        const data = await petugas_parkir.findByPk(id)
-        if(!data) {
-            return res.status(404).json({
-                message: "Data Petugas Tidak Ditemukan"
-            })
-        }
-    
-        if (action === 'Approve') {
-    
-            data.status_post = 'Approved'
-            await data.save()
-            return res.status(200).json({
-                message: "Data Petugas Parkir di Setujui",
-                status_post: data.status_post
-            })
-            
-        }else if (action === 'Reject'){
-            data.status_post = 'Rejected'
-            await data.save()
-            return res.status(200).json({
-                message: "Data Petugas Parkir di Tolak",
-                status_post: data.status_post
-            })
-        } else {
-            return res.status(404).json({
-                message: 'Action harus berupa "Approve" atau "Reject"'
-            })
-            
-        }
-        await data.save()
-        res.status(200).json({
-            message: "Data Petugas Parkir telah $ {data.status_post"
-        })
+    const { id } = req.params;
+    const { action } = req.body; // 'approve' atau 'reject'
+
+    const data = await petugas_parkir.findByPk(id);
+    if (!data) {
+        return res.status(404).json({ message: 'Data petugas tidak ditemukan.' });
+    }
+
+    if (action === 'Approve') {
+        data.status_post = 'Approved'; // Ubah status_post menjadi Approved
+    } else if (action === 'Reject') {
+        data.status_post = 'Rejected'; // Ubah status_post menjadi Rejected
+    } else {
+        return res.status(400).json({ message: 'Action harus berupa "Approve" atau "Reject".' });
+    }
+
+    await data.save();
+    res.status(200).json({
+        message: `Data petugas parkir telah ${data.status_post}.`,
+        status_post: data.status_post
+    });
     
         
     }
