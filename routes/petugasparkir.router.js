f85204da877d4476ded0d780392e0fa290fae3de
GLynn <febriantomanangi@gmail.com>
2024-10-21 21:07:49 +0800

    add route

--- routes/petugasparkir.router.js
+++ routes/petugasparkir.router.js
@@ -16,7 +16,7 @@ const upload = require('../middleware/upload')
 const {checkPetugasParkirStatus} = require('../controllers/petugasparkir.controller')
 
 router.get("/petugas", verifyUser, isUser,  getAllPetugas)
-router.get("/petugas", verifyUser, isUser,  getPetugasById)
+router.get("/petugas/:id", verifyUser, isUser,  getPetugasById)
 router.get("/petugas/status/:id", verifyUser, isUser, checkPetugasParkirStatus)
 router.post("/petugas", verifyUser, isUser, upload.single('bukti'), addPetugas);
 router.patch("/petugas/:id", verifyUser, isUser,  upload.single('bukti'), updatePetugas )
