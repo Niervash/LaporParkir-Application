6cd9596b0a9fc44ea108c1abb3920a84f83f4c98
Julian Witjaksono <151512615+Niervash@users.noreply.github.com>
2024-10-23 01:37:13 +0800

    Delete FE directory

--- FE/src/component/Fragment/Dashboard/layoutpelapor/sidebar/SidebarPelapor.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const SidebarPelapor = () => {
-  return (
-    <div>NavbarPelapor</div>
-  )
-}
--- FE/src/content/auth/API/API_PATH.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const API_PATH = () => {
-  const api = require('
-  return (
-    <div>API_PATH</div>
-  )
-}
